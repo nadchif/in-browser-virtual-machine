505df34fb34d9cae96279e52d526abca3d1534c7
nadchif <nadchif@gmail.com>
2024-10-20 16:26:24 +0800

    Configure alpine setup to use xtermjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from 'react';
+import { Terminal } from '@xterm/xterm';
 
-function App(){
+const term = new Terminal();
+function App() {
+  const terminalDivRef = useRef(null);
 
-useEffect(function  initializeEmulator(){
-  // See https://github.com/copy/v86/blob/master/src/browser/starter.js for options 
-window.emulator = new window.V86({
-  wasm_path:  '/v86.wasm',
-  screen_container:  document.getElementById("screen_container"),
-  bios: {
-    url:  "/bios/seabios.bin",
-  },
-  vga_bios: {
-    url:  "/bios/vgabios.bin",
-  },
-  boot_order: '0x123', // Boot from CD-ROM first
-  memory_size: 512 * 1024 * 1024, // 512MB RAM
-  vga_memory_size: 64 * 1024 * 1024, // 64MB VGA RAM
-  // See more: https://github.com/copy/v86/blob/master/docs/networking.md
-  net_device: {
-    type: 'virtio',
-    relay_url: "wisps://wisp.mercurywork.shop",
-  },
-  cdrom: {
-    // Source: https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86/alpine-virt-3.20.3-x86.iso
-    url: "/images/alpine-virt-3.20.3-x86.iso", 
-  },
-  autostart:  true,
-});
-}, []);
+  useEffect(function initializeEmulator() {
+    if (!window.emulator) {
+      term.write('Loading emulator. Please wait it may take a few minutes to boot...\n');
+      // See https://github.com/copy/v86/blob/master/src/browser/starter.js for options
+      window.emulator = new window.V86({
+        wasm_path: '/v86.wasm',
+        screen_container: document.getElementById('screen_container'),
+        bios: {
+          url: '/bios/seabios.bin',
+        },
+        vga_bios: {
+          url: '/bios/vgabios.bin',
+        },
+        boot_order: '0x123', // Boot from CD-ROM first
+        memory_size: 512 * 1024 * 1024, // 512MB RAM
+        vga_memory_size: 64 * 1024 * 1024, // 64MB VGA RAM
+        // See more: https://github.com/copy/v86/blob/master/docs/networking.md
+        net_device: {
+          type: 'virtio',
+          relay_url: 'wisps://wisp.mercurywork.shop',
+        },
+        cdrom: {
+          // Source: https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86/alpine-virt-3.20.3-x86.iso
+          url: '/images/alpine-virt-3.20.3-x86.iso',
+        },
+        autostart: true,
+      });
+      term.open(terminalDivRef.current);
+      term.onKey((e) => window.emulator.serial0_send(e.key));
+      window.emulator.add_listener('serial0-output-byte', (byte) =>
+        term.write(String.fromCharCode(byte))
+      );
+    }
+  }, []);
 
-  return (
-    <div id="screen_container">
-      <div id="screen" style={{overflow: 'hidden'}}>Initializing Emulator...</div>
-      <canvas style={{display: 'none'}}></canvas>
-    </div>
-    );
-  }
-  export default App
+  return <div ref={terminalDivRef} id="terminal"></div>;
+}
+export default App;
